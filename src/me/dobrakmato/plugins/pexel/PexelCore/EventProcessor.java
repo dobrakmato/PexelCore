2ce436ae614ddb596c44019f4ed76f4a9762e8e2
dobrakmato <dobrakmato@zoho.com>
2014-08-16 11:24:30 +0200

    adf

--- src/me/dobrakmato/plugins/pexel/PexelCore/EventProcessor.java
+++ src/me/dobrakmato/plugins/pexel/PexelCore/EventProcessor.java
@@ -48,8 +48,8 @@ public class EventProcessor implements Listener
 							0.20F,
 							0.50F,
 							1,
-							(int) Math.floor(0.010 * event.getTo().distance(
-									event.getFrom())));
+							(int) Math.floor(0.005 * Math.pow(
+									event.getTo().distance(event.getFrom()), 2)));
 				}
 			}
 		}
