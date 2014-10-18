c6b1cfecc3ff5910094fd9f97a13b7df1eca0a1d
dobrakmato <dobrakmato@zoho.com>
2014-10-19 01:22:23 +0200

    added achievement description

--- src/eu/matejkormuth/pexel/PexelCore/core/Achievement.java
+++ src/eu/matejkormuth/pexel/PexelCore/core/Achievement.java
@@ -8,7 +8,15 @@ import eu.matejkormuth.pexel.PexelCore.minigame.Minigame;
 public class Achievement {
     private String name;
     private String displayName;
+    private String description;
     private int    maxProgress;
+    
+    /**
+     * Private constructor. Please use static methods.
+     */
+    private Achievement() {
+        
+    }
     
     /**
      * Builds achievement for specified minigame with specified name and display name.
@@ -16,16 +24,21 @@ public class Achievement {
      * @param minigame
      *            minigame
      * @param name
-     *            name of achievement (will be automatically prefixed with '{minigamename}.'.
+     *            name of achievement (will be automatically prefixed with '{minigamename}.').
+     * @param description
+     *            description of achievement
      * @param displayName
      *            display name
+     * @param maxProgress
+     *            maximum progress of achievement
      * @return achievement
      */
     public static Achievement minigame(final Minigame minigame, final String name,
-            final String displayName, final int maxProgress) {
+            final String description, final String displayName, final int maxProgress) {
         Achievement ach = new Achievement();
         ach.name = minigame.getName() + "." + name;
         ach.displayName = displayName;
+        ach.description = description;
         ach.maxProgress = maxProgress;
         return ach;
     }
@@ -34,16 +47,21 @@ public class Achievement {
      * Builds global achievement that applies to whole network with specified name and display name.
      * 
      * @param name
-     *            name (will be automatically prefixed with 'global.'.
+     *            name (will be automatically prefixed with 'global.').
      * @param displayName
      *            display name
+     * @param description
+     *            description of achievement
+     * @param maxProgress
+     *            maximum progress of achievement
      * @return achievement
      */
     public static Achievement global(final String name, final String displayName,
-            final int maxProgress) {
+            final String description, final int maxProgress) {
         Achievement ach = new Achievement();
         ach.name = "global." + name;
         ach.displayName = displayName;
+        ach.description = description;
         ach.maxProgress = maxProgress;
         return ach;
     }
@@ -55,6 +73,15 @@ public class Achievement {
      */
     public String getName() {
         return this.name;
+    }
+    
+    /**
+     * Returns the description.
+     * 
+     * @return the description
+     */
+    public String getDescription() {
+        return this.description;
     }
     
     /**
