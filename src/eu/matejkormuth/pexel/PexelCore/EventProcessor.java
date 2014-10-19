fbb570ed16ea995f06230f96beceafcd5910a29a
dobrakmato <dobrakmato@zoho.com>
2014-10-19 18:07:11 +0200

    fixéd event processór - fall dmg on lobbies

--- src/eu/matejkormuth/pexel/PexelCore/EventProcessor.java
+++ src/eu/matejkormuth/pexel/PexelCore/EventProcessor.java
@@ -37,6 +37,7 @@ import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.EntityDamageByBlockEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
@@ -141,6 +142,14 @@ public class EventProcessor implements Listener {
     
     @EventHandler
     private void onPlayerDamageByBlock(final EntityDamageByBlockEvent event) {
+        if (event.getEntity() instanceof Player)
+            if (!this.hasPermission(event.getEntity().getLocation(),
+                    (Player) event.getEntity(), AreaFlag.PLAYER_GETDAMAGE))
+                event.setCancelled(true);
+    }
+    
+    @EventHandler
+    private void onPlayerDamage(final EntityDamageEvent event) {
         if (event.getEntity() instanceof Player)
             if (!this.hasPermission(event.getEntity().getLocation(),
                     (Player) event.getEntity(), AreaFlag.PLAYER_GETDAMAGE))
